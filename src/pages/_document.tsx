e338c70600ac272b5593b971ea712fb126bd16f7
Naoto Kamba <naoto.kamba@outlook.jp>
2022-07-14 00:22:04 +0900

    Revert "gtm試し再"
    This reverts commit 4d7afe43a11785221b0314b2e8258af0489bcde2.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,19 +16,17 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <Script
-            id="google-tagmanager"
-            strategy="afterInteractive"
+          <script
             dangerouslySetInnerHTML={{
               __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5XCJSWT');
-          `,
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5XCJSWT');
+    `,
             }}
-          ></Script>
+          />
         </Head>
         <body>
           <noscript>
