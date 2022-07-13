5ad8d446b9461eebfa3983d8c158bf7ea1d3b55c
Naoto Kamba <naoto.kamba@outlook.jp>
2022-07-14 00:34:35 +0900

    Revert "gtm試し再"
    This reverts commit 8b672fc74df068f19ab81b65ff18992ce68bde8a.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,13 +16,12 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <Script />
           <script
             dangerouslySetInnerHTML={{
               __html: `
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;defer=true;j.src=
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5XCJSWT');
     `,
